ad3449cb9894f485036e50ab6304bfc25bc20b9b
mitchell <rksas927@gmail.com>
2023-04-20 22:47:22 +0900

    Added basic alerts when pz create.

--- frontend/app/src/pages/pzs/new.tsx
+++ frontend/app/src/pages/pzs/new.tsx
@@ -2,16 +2,21 @@ import React, { useState } from "react";
 import * as THREE from "three";
 import { GLTFExporter } from "three/examples/jsm/exporters/GLTFExporter";
 import axios from "../../../lib/axiosInstance";
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
+import { TextField, Button, Box, Grid, Alert, Snackbar } from "@mui/material";
 
 const NewPz: React.FC = () => {
+  // Pz Data
   const [latitude, setLatitude] = useState("0");
   const [longitude, setLongitude] = useState("0");
   const [radius, setRadius] = useState("0");
   const [altitude, setAltitude] = useState("0");
+  // Basic Alerts
+  const [alertOpen, setAlertOpen] = useState(false);
+  const [alertSeverity, setAlertSeverity] = useState("info");
+
+  const handleCloseAlert = () => {
+    setAlertOpen(false);
+  };
 
   const btnClick = async (event: React.FormEvent) => {
     event.preventDefault(); // ページリロードを防ぐ
@@ -19,10 +24,17 @@ const NewPz: React.FC = () => {
     const result = await createPz();
   
     if (result) {
-      await exportCylinder();
+      setAlertSeverity("info");
     } else {
-      console.error('Failed create pz.');
+      setAlertSeverity("error");
     }
+    // アラートを表示する
+    setAlertOpen(true);
+
+    // アラートを3秒後に自動的に閉じる
+    setTimeout(() => {
+      setAlertOpen(false);
+    }, 3000);
   };
 
   const createPz = async (): Promise<boolean> => {
@@ -36,8 +48,6 @@ const NewPz: React.FC = () => {
         radius: parseFloat(radius),
         altitude: parseFloat(altitude),
       });
-      console.log("response");
-      console.log(response);
 
       if (response.status === 200 || response.status === 201) {
         return true;
@@ -62,9 +72,6 @@ const NewPz: React.FC = () => {
       opacity: 0.4,
     });
     const cylinder = new THREE.Mesh(geometry, material);
-
-    console.log('cylinder')
-    console.log(cylinder)
 
     // Export the 3D object in GLTF format and send it to the server
     const exporter = new GLTFExporter();
@@ -98,55 +105,69 @@ const NewPz: React.FC = () => {
   };
 
   return (
-    <Box component="div">
-      <form onSubmit={btnClick}>
-        <Grid container spacing={2}>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              label="緯度"
-              type="number"
-              value={latitude}
-              onChange={(e) => setLatitude(e.target.value)}
-              fullWidth
-            />
+    <div>
+      <Box component="div">
+        <form onSubmit={btnClick}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                label="緯度"
+                type="number"
+                value={latitude}
+                onChange={(e) => setLatitude(e.target.value)}
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                label="経度"
+                type="number"
+                value={longitude}
+                onChange={(e) => setLongitude(e.target.value)}
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                label="半径(m)"
+                type="number"
+                value={radius}
+                onChange={(e) => setRadius(e.target.value)}
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                label="高さ(m)"
+                type="number"
+                value={altitude}
+                onChange={(e) => setAltitude(e.target.value)}
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Box display="flex" justifyContent="flex-end">
+                <Button variant="contained" type="submit">
+                  登録
+                </Button>
+              </Box>
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              label="経度"
-              type="number"
-              value={longitude}
-              onChange={(e) => setLongitude(e.target.value)}
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              label="半径(m)"
-              type="number"
-              value={radius}
-              onChange={(e) => setRadius(e.target.value)}
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              label="高さ(m)"
-              type="number"
-              value={altitude}
-              onChange={(e) => setAltitude(e.target.value)}
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Box display="flex" justifyContent="flex-end">
-              <Button variant="contained" type="submit">
-                登録
-              </Button>
-            </Box>
-          </Grid>
-        </Grid>
-      </form>
-    </Box>
+        </form>
+      </Box>
+      <Snackbar
+        open={alertOpen}
+        autoHideDuration={3000}
+        onClose={handleCloseAlert}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      >
+        <Alert onClose={handleCloseAlert} severity={alertSeverity}>
+          {alertSeverity === "info"
+            ? "Pz was successfully created."
+            : "Failed to create Pz."}
+        </Alert>
+      </Snackbar>
+    </div>
   );
 };
 
