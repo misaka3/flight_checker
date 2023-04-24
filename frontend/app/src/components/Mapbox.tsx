c70d56e6c1fd2cbf7518b2c378e0807dde9a2f63
mitchell <rksas927@gmail.com>
2023-04-24 22:22:41 +0900

    Add map's center LatLon on pzs/new page

--- frontend/app/src/components/Mapbox.tsx
+++ frontend/app/src/components/Mapbox.tsx
@@ -3,6 +3,7 @@ import ReactMap, { AttributionControl, ViewState } from "react-map-gl";
 import DeckGL from "@deck.gl/react/typed";
 import { ColumnLayer } from "@deck.gl/layers/typed";
 import { latLonToMGRS } from '../utils/coordinateUtils';
+import { Box, Grid, TextField } from "@mui/material";
 
 const MAPBOX_ACCESS_TOKEN = process.env.NEXT_PUBLIC_MapboxAccessToken;
 
@@ -59,8 +60,8 @@ const Mapbox: React.FC<MapboxProps> = ({ objects = [] }) => {
   };
 
   return (
-    <div style={{ width: "100%", height: "100%" }}>
-      {objects.length > 0 ? (
+    objects.length > 0 ? (
+      <div style={{ width: "100%", height: "100%" }}>
         <DeckGL
           layers={layers}
           initialViewState={initialViewState}
@@ -74,16 +75,41 @@ const Mapbox: React.FC<MapboxProps> = ({ objects = [] }) => {
             attributionControl={false}
           />
         </DeckGL>
-      ) : (
-        <ReactMap
-          {...viewState}
-          mapboxAccessToken={MAPBOX_ACCESS_TOKEN}
-          mapStyle="mapbox://styles/mapbox/streets-v11"
-          attributionControl={false}
-          onMove={evt => setViewState(evt.viewState)}
-        />
-      )}
-    </div>
+      </div>
+    ) : (
+      <div style={{ width: "100%", height: "100%", display: "flex", flexDirection: "column" }}>
+        <Box component="div" sx={{ marginBottom: "16px"}}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                label="緯度"
+                type="number"
+                value={viewState.longitude}
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                label="経度"
+                type="number"
+                value={viewState.latitude}
+                fullWidth
+              />
+            </Grid>
+          </Grid>
+        </Box>
+        <div style={{ flexGrow: 1 }}>
+          <ReactMap
+            {...viewState}
+            mapboxAccessToken={MAPBOX_ACCESS_TOKEN}
+            mapStyle="mapbox://styles/mapbox/streets-v11"
+            attributionControl={false}
+            onMove={evt => setViewState(evt.viewState)}
+          />
+        </div>
+      </div>
+
+    )
   );
 };
 
