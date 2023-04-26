96a4ce78d06b61a85c600c9224d53ae1f9b926c5
mitchell <rksas927@gmail.com>
2023-04-26 22:28:35 +0900

    Moved columnLayer creation method to utils.

--- frontend/app/src/utils/layerUtils.ts
+++ frontend/app/src/utils/layerUtils.ts
@@ -1,5 +1,26 @@
 import { PathLayer } from '@deck.gl/layers/typed';
 import { PathStyleExtension } from '@deck.gl/extensions/typed';
+import { ColumnLayer } from "@deck.gl/layers/typed";
+
+interface PzArrayObject {
+  coordinates: [number, number];
+  radius: number;
+  altitude: number;
+}
+
+// cylinder object
+export function createColumnLayer(pz: PzArrayObject) {
+  return new ColumnLayer({
+    id: `column-layer-${pz.coordinates}`,
+    data: [pz],
+    getPosition: (d: PzArrayObject) => d.coordinates,
+    getFillColor: [255, 0, 0, 255 * 0.5],
+    radius: pz.radius,
+    // change altitude to meters from feet
+    getElevation: (d: PzArrayObject) => d.altitude / 3.28084,
+    pickable: true,
+  });
+}
 
 export function altitudeToColor(altitude: number): [number, number, number] {
   // min, max altitude feet
@@ -14,7 +35,7 @@ export function altitudeToColor(altitude: number): [number, number, number] {
   return [r, g, b];
 }
 
-// Create a path layer(gpx track)
+// gpx track
 export function createPathLayer(gpxDatas: any[]) {
   const pathLayer = new PathLayer({
     id: 'path-layer',
