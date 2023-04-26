c82abf1e6ad4ffe0c56053e9179133ac638a04aa
mitchell <rksas927@gmail.com>
2023-04-26 17:32:52 +0900

    Moved pathLayer creation method to utils.

---
+++ frontend/app/src/utils/layerUtils.ts
@@ -0,0 +1,33 @@
+import { PathLayer } from '@deck.gl/layers/typed';
+import { PathStyleExtension } from '@deck.gl/extensions/typed';
+
+export function altitudeToColor(altitude: number): [number, number, number] {
+  // min, max altitude feet
+  const minAltitude = 0;
+  const maxAltitude = 4000;
+
+  const t = (altitude - minAltitude) / (maxAltitude - minAltitude);
+  const r = t * 255;
+  const g = 0;
+  const b = (1 - t) * 255;
+
+  return [r, g, b];
+}
+
+// Create a path layer(gpx track)
+export function createPathLayer(gpxDatas: any[]) {
+  const pathLayer = new PathLayer({
+    id: 'path-layer',
+    data: gpxDatas,
+    getPath: (d: any) => d.geometry.coordinates,
+    getColor: (d: any) =>
+      d.geometry.coordinates.map((coordinate: any) =>
+        altitudeToColor(coordinate[2] * 3.28084), // meters to feet
+      ),
+    getWidth: 20,
+    extensions: [new PathStyleExtension({ dash: true })],
+    getDashArray: (d: any) => [0, 0],
+  });
+
+  return pathLayer;
+}
