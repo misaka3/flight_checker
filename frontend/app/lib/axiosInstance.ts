0dbaa5d40fd509f5ab4954d1665ba69a5d3b4edc
mitchell <rksas927@gmail.com>
2023-04-29 23:15:42 +0900

    Change settings for connection.

--- frontend/app/lib/axiosInstance.ts
+++ frontend/app/lib/axiosInstance.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3001'
+  baseURL: process.env.NEXT_PUBLIC_API_ENDPOINT
 });
 
 export default axiosInstance;
