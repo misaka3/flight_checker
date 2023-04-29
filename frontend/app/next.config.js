3f554c2da5e7aa7c29ea8c68dc062f7f90a5461d
mitchell <rksas927@gmail.com>
2023-04-29 12:35:21 +0900

    Fix root path

--- frontend/app/next.config.js
+++ frontend/app/next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback.fs = false;
     }
+    config.resolve.alias['~'] = path.join(__dirname, 'src')
 
     config.module.rules.push({
       test: /\.m?js/,
