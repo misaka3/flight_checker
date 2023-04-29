f13789a407fe32518e70f31472ad19ed2a31cacc
mitchell <rksas927@gmail.com>
2023-04-30 01:41:59 +0900

    Delete foreign_key

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -51,6 +51,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_26_141030) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "events", "areas"
-  add_foreign_key "prohibited_zones", "areas"
 end
