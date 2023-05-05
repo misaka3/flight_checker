29ca1f75946f56fc05fa271945a07952046f3119
mitchell <rksas927@gmail.com>
2023-05-06 00:52:49 +0900

    Create task_rule table and seed file.

---
+++ backend/app/models/task_rule.rb
@@ -0,0 +1,2 @@
+class TaskRule < ApplicationRecord
+end
--- backend/db/migrate/20230417152327_create_task_types.rb
+++ backend/db/migrate/20230417152327_create_task_types.rb
@@ -2,7 +2,9 @@ class CreateTaskTypes < ActiveRecord::Migration[7.0]
   def change
     create_table :task_types do |t|
       t.string :name, null: false, unique: true
+      t.string :en_name, null: false, unique: true
       t.string :short_name, null: false, unique: true
+      t.string :rule_num, null: false, unique: true
       t.string :description
 
       t.timestamps
