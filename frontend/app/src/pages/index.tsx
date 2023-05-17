9d4cf6a98a88f8d1fadd29a79cfafd03d31d201b
mitchell <rksas927@gmail.com>
2023-05-18 01:38:56 +0900

    Create RootPage.

--- frontend/app/src/pages/index.tsx
+++ frontend/app/src/pages/index.tsx
@@ -1,4 +1,31 @@
-export default function Home() {
+import AttachFileIcon from '@mui/icons-material/AttachFile';
+import { Box, Button, Grid, IconButton, TextField } from '@mui/material';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import styles from '~/styles/pages/index.module.css';
+
+const RootPage = () => {
+  const router = useRouter();
+  const [file, setFile] = useState<File | null>(null);
+
+  const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFile(e.target.files?.[0] || null);
+  };
+
+  const handleButtonClick = () => {
+    // !fileの場合、ファイルを選択してくださいというアラートを出す
+    if (!file) {
+      alert("ファイルを選択してください");
+      return;
+    }
+  
+    // /flights/view.tsxへ遷移し、fileを引数(file)として渡す
+    router.push({
+      pathname: '/flights/view',
+      query: { file: file.name },  // 通常、ファイル自体はURLパラメータとして送信できません。なのでここではファイル名を送信しています。
+    });
+  };
+
   return (
     <div style={{
       backgroundImage: "url('/sky_00136.jpg')",
@@ -6,8 +33,57 @@ export default function Home() {
       backgroundPosition: "center",
       height: "100vh",
       width: "100vw",
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
     }}>
-      test
+      <div style={{ width: "60%", maxWidth: "550px", margin: "auto" }}>
+        <Box mb={3}>
+          <Grid container spacing={2} alignItems="center">
+            <Grid item>
+              <input
+                id="file-upload-button"
+                type="file"
+                accept=".gpx"
+                onChange={handleFileUpload}
+                style={{ display: 'none' }}
+              />
+              <label htmlFor="file-upload-button">
+                <IconButton color="default" component="span" aria-label="upload file" sx={{color: 'white'}}>
+                  <AttachFileIcon />
+                </IconButton>
+              </label>
+            </Grid>
+            <Grid item xs>
+              <TextField
+                variant="outlined"
+                disabled
+                value={file ? file.name : '.gpxファイルを選択してください'}
+                fullWidth
+                InputProps={{
+                  style: {
+                    height: '40px',
+                    padding: '0 12px',
+                    backgroundColor: 'white',
+                  },
+                }}
+              />
+            </Grid>
+          </Grid>
+        </Box>
+        <Grid container alignItems="center" justifyContent="center">
+          <Grid item>
+            <Button
+              onClick={() => handleButtonClick()}
+              className={styles.gradient}
+            >
+              航跡図を描画
+            </Button>
+          </Grid>
+        </Grid>
+      </div>
     </div>
   )
 }
+
+export default RootPage;
